8a212ccb100edc1aeaf74ff1c7dfc13489c3e629
Gabe Lerner <gabelerner@gmail.com>
2015-06-21 19:04:21 -0500

    fixes https://github.com/gabelerner/canvg/issues/367

--- canvg.js
+++ canvg.js
@@ -835,7 +835,7 @@
 			this.addStylesFromStyleDefinition = function () {
 				// add styles
 				for (var selector in svg.Styles) {
-					if (matchesSelector(node, selector)) {
+					if (selector[0] != '@' && matchesSelector(node, selector)) {
 						var styles = svg.Styles[selector];
 						var specificity = svg.StylesSpecificity[selector];
 						if (styles != null) {
