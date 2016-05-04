938890265e5ba16b8be4777e91719f20b5608460
Stephen Anders <stephen.anders@culpepper.com>
2016-05-04 13:04:07 -0400

    fix for issue #454
    stroke-dasharray appeared to not work on path elements when the path drawn was a straight line.  This was caused by the call to ctx.closePath() which drew another line back to the origin on top of the original line.  The result looked like a solid line rather than a dashed line.
    
    Fix is to only close the path if it is not a straight line.

--- canvg.js
+++ canvg.js
@@ -1626,7 +1626,12 @@
 						break;
 					case 'Z':
 					case 'z':
-						if (ctx != null) ctx.closePath();
+						if (ctx != null) {
+							//only close path if it is not a straight line
+							if(bb.x1 != bb.x2 && bb.y1 != bb.y2) {
+								ctx.closePath();
+							}
+						}
 						pp.current = pp.start;
 					}
 				}
