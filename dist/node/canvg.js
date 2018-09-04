c89f8f53abbc01bf24ae7bc70c1a426501cb88cc
Mihail Buyskih <mihail@mixbook.com>
2018-09-04 15:44:41 +0300

    Fix IE11/Edge17 isseu when elem has no transformations

--- dist/node/canvg.js
+++ dist/node/canvg.js
@@ -736,7 +736,14 @@
 	      }
 	    };
 
-	    var data = svg.trim(svg.compressSpaces(v)).replace(/\)([a-zA-Z])/g, ') $1').replace(/\)(\s?,\s?)/g, ') ').split(/\s(?=[a-z])/);
+	    var data = svg
+	      .trim(svg.compressSpaces(v))
+	      .replace(/\)([a-zA-Z])/g, ') $1')
+	      .replace(/\)(\s?,\s?)/g, ') ')
+	      .split(/\s(?=[a-z])/)
+	      .filter(function (d) {
+	        return d !== 'none';
+	      });
 	    for (var i = 0; i < data.length; i++) {
 	      var type = svg.trim(data[i].split('(')[0]);
 	      var s = data[i].split('(')[1].replace(')', '');
@@ -1035,7 +1042,7 @@
 	        var gaps = svg.ToNumberArray(this.style('stroke-dasharray').value);
 	        if (typeof ctx.setLineDash != 'undefined') { ctx.setLineDash(gaps); } else if (typeof ctx.webkitLineDash != 'undefined') { ctx.webkitLineDash = gaps; } else if (typeof ctx.mozDash != 'undefined' && !(gaps.length == 1 && gaps[0] == 0)) { ctx.mozDash = gaps; }
 
-	        var offset = this.style('stroke-dashoffset').numValueOrDefault(1);
+	        var offset = this.style('stroke-dashoffset').toPixels();
 	        if (typeof ctx.lineDashOffset != 'undefined') { ctx.lineDashOffset = offset; } else if (typeof ctx.webkitLineDashOffset != 'undefined') { ctx.webkitLineDashOffset = offset; } else if (typeof ctx.mozDashOffset != 'undefined') { ctx.mozDashOffset = offset; }
 	      }
 
@@ -1244,7 +1251,7 @@
 
 	      if (ctx != null) {
 	        ctx.beginPath();
-	        ctx.arc(cx, cy, r, 0, Math.PI * 2, true);
+	        ctx.arc(cx, cy, r, 0, Math.PI * 2, false);
 	        ctx.closePath();
 	      }
 
@@ -1267,11 +1274,11 @@
 
 	      if (ctx != null) {
 	        ctx.beginPath();
-	        ctx.moveTo(cx, cy - ry);
-	        ctx.bezierCurveTo(cx + (KAPPA * rx), cy - ry, cx + rx, cy - (KAPPA * ry), cx + rx, cy);
+	        ctx.moveTo(cx + rx, cy);
 	        ctx.bezierCurveTo(cx + rx, cy + (KAPPA * ry), cx + (KAPPA * rx), cy + ry, cx, cy + ry);
 	        ctx.bezierCurveTo(cx - (KAPPA * rx), cy + ry, cx - rx, cy + (KAPPA * ry), cx - rx, cy);
 	        ctx.bezierCurveTo(cx - rx, cy - (KAPPA * ry), cx - (KAPPA * rx), cy - ry, cx, cy - ry);
+	        ctx.bezierCurveTo(cx + (KAPPA * rx), cy - ry, cx + rx, cy - (KAPPA * ry), cx + rx, cy);
 	        ctx.closePath();
 	      }
 
@@ -1683,7 +1690,12 @@
 	            break;
 	          case 'Z':
 	          case 'z':
-	            if (ctx != null) ctx.closePath();
+	            if (ctx != null) {
+	              // only close path if it is not a straight line
+					      if (bb.x1 !== bb.x2 && bb.y1 !== bb.y2) {
+	                ctx.closePath();
+	              }
+	            }
 	            pp.current = pp.start;
 	        }
 	      }
@@ -2614,6 +2626,12 @@
 	      if (element != null) element.path(ctx);
 	    };
 
+	    this.elementTransform = function () {
+	      if (element != null && element.style('transform', false, true).hasValue()) {
+	        return new svg.Transform(element.style('transform', false, true).value);
+	      }
+	    };
+
 	    this.getBoundingBox = function () {
 	      if (element != null) return element.getBoundingBox();
 	    };
@@ -2717,9 +2735,11 @@
 	      for (var i = 0; i < this.children.length; i++) {
 	        var child = this.children[i];
 	        if (typeof child.path != 'undefined') {
-	          var transform = null;
-	          if (child.style('transform', false, true).hasValue()) {
+	          var transform = typeof child.elementTransform != 'undefined' && child.elementTransform(); // handle <use />
+	          if (!transform && child.style('transform', false, true).hasValue()) {
 	            transform = new svg.Transform(child.style('transform', false, true).value);
+	          }
+	          if (transform) {
 	            transform.apply(ctx);
 	          }
 	          child.path(ctx);
--- src/canvg.js
+++ src/canvg.js
@@ -762,7 +762,14 @@ function build(opts) {
       }
     }
 
-    var data = svg.trim(svg.compressSpaces(v)).replace(/\)([a-zA-Z])/g, ') $1').replace(/\)(\s?,\s?)/g, ') ').split(/\s(?=[a-z])/);
+    var data = svg
+      .trim(svg.compressSpaces(v))
+      .replace(/\)([a-zA-Z])/g, ') $1')
+      .replace(/\)(\s?,\s?)/g, ') ')
+      .split(/\s(?=[a-z])/)
+      .filter(function (d) {
+        return d !== 'none';
+      });
     for (var i = 0; i < data.length; i++) {
       var type = svg.trim(data[i].split('(')[0]);
       var s = data[i].split('(')[1].replace(')', '');
