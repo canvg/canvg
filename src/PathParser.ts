9bbbcfa7f028e61dba218082f789504b2ffcc5cd
Anton Nazarenko <9337044+zarikadzer@users.noreply.github.com>
2021-03-11 14:12:13 +0200

    Update src/PathParser.ts
    Improved whitespace trimming

--- src/PathParser.ts
+++ src/PathParser.ts
@@ -44,7 +44,7 @@ export default class PathParser extends SVGPathData {
 		super(
 			path
 				// Fix spaces after signs.
-				.replace(/[+\-.]\s+/g, '-')
+				.replace(/([+-.])\s+/gm, '$1')
 				// Remove invalid part.
 				.replace(/[^MmZzLlHhVvCcSsQqTtAae\d\s.,+-].*/g, '')
 		);
